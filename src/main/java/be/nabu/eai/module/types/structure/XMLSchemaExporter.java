f90feda49f6c68ea6feecb57b27701e9412286ac
alex <alex@nabu.be>
2016-05-28 15:14:08 +0200

    Added XSD export

---
+++ src/main/java/be/nabu/eai/module/types/structure/XMLSchemaExporter.java
@@ -0,0 +1,41 @@
+package be.nabu.eai.module.types.structure;
+
+import java.io.ByteArrayOutputStream;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.MenuItem;
+import be.nabu.eai.developer.api.EntryContextMenuProvider;
+import be.nabu.eai.developer.util.Confirm;
+import be.nabu.eai.developer.util.Confirm.ConfirmType;
+import be.nabu.eai.repository.api.Entry;
+import be.nabu.libs.types.api.ComplexType;
+import be.nabu.libs.types.definition.xsd.XSDDefinitionMarshaller;
+
+public class XMLSchemaExporter implements EntryContextMenuProvider {
+
+	@Override
+	public MenuItem getContext(final Entry entry) {
+		if (entry.isNode() && ComplexType.class.isAssignableFrom(entry.getNode().getArtifactClass())) {
+			MenuItem item = new MenuItem("Export as XML Schema");
+			item.addEventHandler(ActionEvent.ANY, new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					try {
+						ComplexType complex = (ComplexType) entry.getNode().getArtifact();
+						XSDDefinitionMarshaller marshaller = new XSDDefinitionMarshaller();
+						ByteArrayOutputStream output = new ByteArrayOutputStream();
+						marshaller.marshal(output, complex);
+						Confirm.confirm(ConfirmType.INFORMATION, "XML Schema: " + entry.getId(), new String(output.toByteArray(), "UTF-8"), null);
+					}
+					catch (Exception e) {
+						throw new RuntimeException(e);
+					}
+				}
+			});
+			return item;
+		}
+		return null;
+	}
+	
+}
